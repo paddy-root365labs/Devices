65164eb9f9dd9168ae38857fc330f5ef0821c99a
Paddy <137122589+paddy-root365labs@users.noreply.github.com>
2023-06-19 22:51:39 -0700

    Delete 1)	Delete_IntuneDevice-SerialNumberList.ps1

--- Delete Intune Devices /1)	Delete_IntuneDevice-SerialNumberList.ps1
+++
@@ -1,6 +0,0 @@
-$data = get-content -path .\serialNumber.txt
-foreach ($serialNumber in $data)
-{
-    $deviceID = (Get-IntuneManagedDevice -filter "serialNumber eq '$serialNumber'").id
-    Remove-DeviceManagement_ManagedDevices -managedDeviceId $deviceid
-}
